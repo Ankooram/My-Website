c1a623aebbed4dfdf77a17f2ed2b08eeb228bbf7
Ankooram <71071269+Ankooram@users.noreply.github.com>
2020-09-10 15:34:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+/* #d70d25  - crimson
+	#4948d4 - blue
+	#6400ae - indigo
+	#05b3b6 - aqua
+	#222222 - black */
